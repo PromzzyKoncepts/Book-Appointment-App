8bd0c5d01b64f6cea0274fad3f352a9b2c4ab23a
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-04-01 13:14:26 +0200

    Add reservations association

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :city, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :date, presence: true
+  validates :car_id, presence: true
+  validates :user_id, presence: true
 end
