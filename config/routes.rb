bdc5fa0d4652f86bff9acd54706fcbaafdef6b0a
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-04-01 13:19:31 +0200

    rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :cars, only: [:index, :create, :show, :destroy]
+      resources :reservation, only: [:index,:show, :create ,:new,:destroy, :update]
+      resources :users, only: [:index, :create]
+    end
+  end
 end
