1be59764c3916474c89512e09b81921b57fbf9a2
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-04-07 14:51:25 +0200

    Fix server errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ user3 = User.create(name: 'Promise Okechukwu', email: 'okechukwupromise638@gmail
 car1 = Car.create(name: 'Jeep', model: 'Range Rover', description: 'Best car ever', price: 2000, user_id: user1.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718381-cd6cfcff-a804-4d35-95af-705794d4c4a8.png')
 car2 = Car.create(name: 'Tesla', model: 'Model X', description: 'With lasting battery', price: 14000, user_id: user2.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718933-f55aea72-e56c-4f14-9032-a4902203f77a.png')
 car3 = Car.create(name: 'Toyota', model: 'Carina', description: 'A very confortable car', price: 13250, user_id: user3.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718657-5d0b9f51-8a97-4045-a14a-c70913891275.png')
+car3 = Car.create(name: 'Lamborghini', model: 'lambo', description: 'A very luxarious car', price: 13250, user_id: user1.id, image_url: 'https://user-images.githubusercontent.com/65233860/178718657-5d0b9f51-8a97-4045-a14a-c70913891275.png')
 
 reservation1 = Reservation.create(pickup_date: '2020-02-02', return_date: '2023-01-04', city: 'Bujumbura', user_id: user1.id, car_id: car1.id)
 reservation2 = Reservation.create(pickup_date: '2020-02-02', return_date: '2023-01-04', city: 'Danmark', user_id: user2.id, car_id: car2.id)
