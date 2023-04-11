8bef005aa20aafbf5534d87412438ea46ec88fbf
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-11 12:17:49 +0100

    implement the testing for cars

---
+++ spec/routes/car_routes_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "API V1 Routes", type: :routing do
+  describe "Cars routes" do
+    it "routes to cars#index" do
+      expect(get: "/api/v1/cars").to route_to("api/v1/cars#index", format: 'json')
+    end
+
+    it "routes to cars#new" do
+      expect(get: "/api/v1/cars/new").to route_to("api/v1/cars#new", format: 'json')
+    end
+
+    it "routes to cars#create" do
+      expect(post: "/api/v1/cars").to route_to("api/v1/cars#create", format: 'json')
+    end
+
+    it "routes to cars#show" do
+      expect(get: "/api/v1/cars/1").to route_to("api/v1/cars#show", id: "1", format: 'json')
+    end
+  end
+end
