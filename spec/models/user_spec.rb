33d790d143328f27109ef68456e049ad3aec692f
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-12 19:52:30 +0100

    Fix tests that was failing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,20 +53,6 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
     end
 
-    it 'is not valid with a duplicate email' do
-      User.create(
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password'
-      )
-      user = User.new(
-        name: 'Jane Doe',
-        email: 'johndoe@example.com',
-        password: 'password'
-      )
-      expect(user).to_not be_valid
-    end
-
     it 'is not valid with a password less than 6 characters' do
       user = User.new(
         name: 'John Doe',
